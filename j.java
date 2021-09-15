320cbd49831ec2b4a2586435fa443d76b423a9e6
Vinnakotarohith <90406226+Vinnakotarohith@users.noreply.github.com>
2021-09-16 00:26:12 +0530

    java
    maximum of two numbers

---
+++ j.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+import java.io.*;
+import java.lang.*;
+import java.applet.*;
+ class j{
+
+   void run(){
+      int a,b;
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the number");
+a=sc.nextInt();
+b=sc.nextInt();
+if (a>b){
+   System.out.println("a is max");
+
+}
+else{
+   System.out.println("b is max");
+}
+
+   }
+   public static void main(String [ ] args){
+   
+j ad=new j();
+     ad. run();
+   }
+ }
+
+
+
+
+
+ 
+  
+    

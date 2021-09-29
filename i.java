61eef7042aa0416a77dc9b633555b72f7b79a323
Vinnakotarohith <90406226+Vinnakotarohith@users.noreply.github.com>
2021-09-29 11:40:41 +0530

    palindrome reverse the string

--- i.java
+++ i.java
@@ -1,35 +1,19 @@
 import java.util.*;
 //check the whether number is palindrome or not
-// A number that is same reverse number 
+// A number that is same reverse word
 class j {
 
   void sa(){
+    String a,reverse="";
+
     Scanner sx=new Scanner(System.in);
-    System.out.println("Enter the number");
+    System.out.println("Enter the string");
 
-    int a=sx.nextInt();// reading the variable
-    int r,sum=0,temp;
-    temp=a;//The given number stored in another variable.
-    while(a>0){
-      /*Example:The given number is "454"
-      first we want the reminder of the given variable--> 
-      10)454(45
-         40
-          50
-         -----
-         4
-         the reminder is 4
-         next we do sum =0*4+4=4
-         and a=454/10=45.4 it takes only integer number only not float values
-         then the given number is "45"
-         and again do same process until the given number is the given number is less than '0'.
-
-      */ 
-    r=a%10;
-    sum=sum*10+r;
-    a=a/10;
-    }
-    if(temp==sum){
+     a=sx.nextLine();// reading the word
+     for(int i=a.length()-1;i>=0;i--)
+     reverse=reverse+a.charAt(i);
+   System.out.println(reverse);
+    if(a.equals(reverse)){
     System.out.println("palindrome");
     }
     else{

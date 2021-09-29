dbd32e64acaa5080e147d0deaa5c152cf19e7001
Vinnakotarohith <90406226+Vinnakotarohith@users.noreply.github.com>
2021-09-29 16:13:07 +0530

    Random number

---
+++ s.java
@@ -0,0 +1,13 @@
+import java.util.*;
+// Random number
+class s {
+    public static void main(String [] args){
+
+    
+    int max=5000,min=1000;
+    System.out.println("max : "+max+" min : "+min);
+    int b=(int)(Math.random()*(max-min+1)+min);
+    System.out.println(b);
+    }
+    
+}
